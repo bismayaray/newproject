127d56633a970d2bed4fd0888e0e8f352e819ade
bisha <bisha@Dell>
2022-10-31 19:43:13 +0530

    Framework commit

---
+++ src/main/java/genericLib/WebDriverUtilities.java
@@ -0,0 +1,68 @@
+package genericLib;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Set;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+public class WebDriverUtilities 
+{
+	public void dropdown(WebElement ele,String text) {
+		Select s=new Select(ele);
+		s.selectByVisibleText(text);
+	}
+	
+	public void mouseHover(WebDriver driver,WebElement ele) {
+		Actions a=new Actions(driver);
+		a.moveToElement(ele).perform();
+	}
+	
+	public void doubleClick(WebDriver driver,WebElement ele) {
+		Actions a=new Actions(driver);
+		a.doubleClick(ele).perform();
+	}
+	
+	public void draganddrop(WebDriver driver,WebElement src,WebElement dest) {
+		Actions a=new Actions(driver);
+		a.dragAndDrop(src,dest).perform();
+	}
+	
+	public void switchFrame(WebDriver driver) {
+		driver.switchTo().frame(0);
+	}
+	
+	public void switchBack(WebDriver driver) {
+		driver.switchTo().defaultContent();
+	}
+	
+	public void alertpopup(WebDriver driver) {
+		driver.switchTo().alert().accept();
+	}
+	
+	
+public void scrollBar(WebDriver driver,int x,int y) {
+	JavascriptExecutor js=(JavascriptExecutor) driver;
+	js.executeScript("window.scrollBy("+x+","+y+")");
+		
+	}
+	
+	public void switchTabs(WebDriver driver) {
+		Set<String> child = driver.getWindowHandles();
+		
+		for(String b:child) {
+			driver.switchTo().window(b);
+		}
+		
+	}
+
+
+}
---
+++ src/test/java/scripts/TestCase3.java
@@ -0,0 +1,35 @@
+package scripts;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+import genericLib.BaseClass;
+import pomPages.CorejavaPage;
+import pomPages.SkillraryLoginPage;
+import pomPages.WishlistPage;
+
+public class TestCase3 extends BaseClass
+{
+	@Test
+	public void tc3() throws IOException, InterruptedException {
+	SkillraryLoginPage sl=new SkillraryLoginPage(driver);
+	sl.serachtextbox(pdata.getData("serach"));
+	sl.serachbutton();
+	
+	CorejavaPage c=new CorejavaPage(driver);
+	c.corejavacourse();
+	
+	WishlistPage w=new WishlistPage(driver);
+	w.closepopup();
+	driverutiles.switchFrame(driver);
+	
+	Thread.sleep(2000);
+	w.playbutton();
+	Thread.sleep(5000);
+	w.pausebtn();
+	driverutiles.switchBack(driver);
+	w.addtowishlidtbtn();
+	}
+
+}
